6ee05a09ea934ea2af790d73038e57c1826ca343
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-07-05 15:37:28 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+
+// Import our custom CSS
+import "~resources/scss/app.scss";
+
+import.meta.glob(["../img/**"]);
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravell Template</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>Teplate Laravel</h1>
+    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella template">
+</body>
+</html>
